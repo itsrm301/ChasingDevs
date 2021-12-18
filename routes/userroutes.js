7d9787d2445e1a8e28c0d6533ac71eeac396c063
itsrm301 <itsrm301@kgpian.iitkgp.ac.in>
2021-12-19 01:24:38 +0530

    Backend-Raghav

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,8 +1,19 @@
 const express=require("express");
 const router=express.Router();
+const getrow = require("../src/fetchcredentials");
+
 
 router.get("/",(req,res)=>{
     res.send("hello just for testing whether working or not");
 });
+
+router.post("/login",(req,res)=>{
+    const user=getrow(req.body.email);
+    console.log(user);
+    if(user!==null){
+        console.log(user);
+        res.send(user);
+    }
+})
 
 module.exports =router;
