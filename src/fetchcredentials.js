1158da9ed20fb4047bd8a5b1e4502a6558190c69
anjaniit23 <anjaniit23@gmai.com>
2021-12-19 03:51:11 +0530

    otp login added

--- src/fetchcredentials.js
+++ src/fetchcredentials.js
@@ -3,11 +3,7 @@ const fs =require("fs");
 const { resolve } = require("path");
 const EXCEL_SHEET_ID="1o8Fe2ZX_YSeOvT6yg4uo2EE9O-oVQ6FNieaHPVMJn5w";
 const doc = new GoogleSpreadsheet(EXCEL_SHEET_ID);
-<<<<<<< HEAD
 const CREDENTIAL = JSON.parse(fs.readFileSync(__dirname+'/googlesheetcredentials.json'));
-=======
-const CREDENTIAL = JSON.parse(fs.readFileSync(__dirname +'/googlesheetcredentials.json', 'utf8'));
->>>>>>> 3e70922d61b1e1423b60a798fe137135c3887960
 
 const getRow=async (email)=>{
     await doc.useServiceAccountAuth({
@@ -24,12 +20,7 @@ const getRow=async (email)=>{
     for( index=0;index<rows.length;index++){
         const row = rows[index];
         if(row.email==email){
-<<<<<<< HEAD
              
-=======
-            console.log(row.name);
-            resolve();
->>>>>>> 3e70922d61b1e1423b60a798fe137135c3887960
             return row;
             }
 
@@ -39,10 +30,6 @@ const getRow=async (email)=>{
     }
 
 }
-<<<<<<< HEAD
- 
-=======
 // getRow('pqr@iitkgp.ac.in').then(email=>console.log(email.email));
->>>>>>> 3e70922d61b1e1423b60a798fe137135c3887960
 
 module.exports= getRow;
