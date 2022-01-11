2d1711c3dbb13d41b30ab60e166b905445393490
Anjani Kumar <w3docs_email@example.com>
2022-01-11 08:15:47 +0530

    demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const express =require("express");
-const mongoose= require("mongoose");
-const userRoutes =require("../routes/userroutes");
-const app= express();
+require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("../routes/userroutes");
+const app = express();
 
-const conndb=async()=>{ return await mongoose.connect(process.env.URI);}
-conndb().then(()=>{console.log("mongDb connected");}).catch(err=>console.log(err));
+const conndb = async () => { return await mongoose.connect(process.env.URI); }
+conndb().then(() => { console.log("mongDb connected"); }).catch(err => console.log(err));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/user",userRoutes);
+app.use("/api/user", userRoutes);
 
 
-const port=process.env.PORT||3000;
-app.listen(port,()=>{console.log(`listenning on port ${port}`)});
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`listenning on port ${port}`) });
